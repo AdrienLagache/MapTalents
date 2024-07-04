6ca10f32aa6ce1256ab19065c3f94e0d6e24d9cf
AdrienLagache <lagacheadrien64@gmail.com>
2024-07-04 19:41:06 +0200

    .

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: 'https://maptalents.website', // Remplacez par votre domaine
+    origin: '*',
+    // origin: 'https://maptalents.website',
+    // origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    credentials: false,
   });
   await app.listen(3000);
 }
